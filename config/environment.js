3979dadf3ea254a093cad87c9cc8f2aea481764d
Halle Williams and Jeremy Fryd <halle+jeremy@>
2016-10-31 10:58:20 -0700

    setup firebase, display categories list on index

---
+++ config/environment.js
@@ -0,0 +1,58 @@
+/* jshint node: true */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'jeffslist',
+    environment: environment,
+    rootURL: '/',
+    locationType: 'auto',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
+      }
+    },
+
+    firebase: {
+      apiKey: "AIzaSyAbXCN5lcziW6KRrN8O_9CIUQsyc-XbdnU",
+      authDomain: "jeffslist-c2940.firebaseapp.com",
+      databaseURL: "https://jeffslist-c2940.firebaseio.com",
+      storageBucket: "jeffslist-c2940.appspot.com",
+    },
+
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    }
+  };
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+
+  }
+
+  return ENV;
+};
